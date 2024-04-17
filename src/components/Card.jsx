a7a76713f6621b49567e67b7ee9046fb543cf9a9
llamouth <150949923+llamouth@users.noreply.github.com>
2024-04-17 12:32:56 -0400

    Complete Assesment

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./styles/style.css"
+import person from "../assets/person.jpg"
 
 const Card = ({canidate}) => {
-  
+
   return (
     <div className='card'>
-      <img src="src/assets/person.jpg" alt="" />
+      <img src={person} alt="" />
       <p>{canidate}</p>
     </div>
   )
