61bc104b495811c239b4b79deb12b881ec27ad49
llamouth <150949923+llamouth@users.noreply.github.com>
2024-04-17 12:29:27 -0400

    Complete Assesment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import "./styles/style.css"
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img src="src/assets/image.png" alt="" />
+      <img src="/Users/larrylamouth/Pursuit/M2/Labs/election-candidates/src/assets/image.png" alt="" />
       </div>
   )
 }
